da6bd83f43141c9aff385c0a1c61a4b124cb0841
Taku Nakagawa <tk@www-biz.co>
2019-11-07 14:10:58 +0900

    取得センサーの個数をボットに表示することが出来た

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const config = {
     channelAccessToken: env.ACCESS_TOKEN
 }
 
+const BufferData = {
+    count0: { value: [0], message: 'センサー 0個', countSensor: 0},
+    count1: { value: [1, 2, 4, 8], message: 'センサー 1個', countSensor: 1},
+    count2: { value: [3, 5, 6, 9, 10, 12], message: 'センサー 2個', countSensor: 2},
+    count3: { value: [7, 11, 13, 14], message: 'センサー 3個', countSensor: 3 },
+    count4: { value: [15], message: 'センサー 4個', countSensor: 4 },
+};
+
 const client = new line.Client(config);
 
 // const app = express();
@@ -21,7 +29,6 @@ app.get('/',  (req, res) => {
 
 app.post('/callback', line.middleware(config), (req, res) => {
     res.sendStatus(200);
-    console.log(req);
     Promise.all(req.body.events.map(handleEvent)).then((result) => res.json(result));
 });
 
@@ -34,18 +41,31 @@ function handleEvent(event) {
 }
 
 function handleThingsEvent(event) {
-    console.log('event', event);
-    const echo =  { type: 'text', text: 'Things Event' };
+    const payLoad = event.things.result.bleNotificationPayload;
+    const buffPayLoad = Buffer.from(payLoad, 'base64');
+    const countSensor = new Int8Array(buffPayLoad);
+    const message = jugmentCountSensor(countSensor[0])
+    const echo = { type: 'text', text: message };
     return client.replyMessage(event.replyToken, echo)
-    .then((res) => console.log('Success: ', res))
-    // .catch((error) => console.log('Error: ', error));
-}
+};
 
 function handleMessageEvent(event) {
     const echo =  { type: 'text', text: 'Message Event' };
     return client.replyMessage(event.replyToken, echo)
-    .then((res) => console.log('Success: ', res))
-    // .catch((error) => console.log('Error: ', error));
-}
+};
+
+function jugmentCountSensor(sensor) {
+    if (BufferData.count0.value.indexOf(sensor) > -1) {
+      return (BufferData.count0.message)
+    } else if (BufferData.count1.value.indexOf(sensor) > -1) {
+      return (BufferData.count1.message)
+    } else if (BufferData.count2.value.indexOf(sensor) > -1) {
+      return (BufferData.count2.message)
+    } else if (BufferData.count3.value.indexOf(sensor) > -1) {
+      return (BufferData.count3.message)
+    } else if (BufferData.count4.value.indexOf(sensor) > -1) {
+      return (BufferData.count4.message)
+    }
+  }
 
 console.log(`Server running at ${PORT}`);
