e04e0c91845dce69b7975946d779caf36834049a
Taku Nakagawa <tk@www-biz.co>
2019-11-07 16:57:48 +0900

    文章内に絵文字を設置した

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const config = {
 }
 
 const BufferData = {
-  count0: { value: [0], message: 'センサー 0個', countSensor: 0 },
-  count1: { value: [1, 2, 4, 8], message: 'センサー 1個', countSensor: 1 },
-  count2: { value: [3, 5, 6, 9, 10, 12], message: 'センサー 2個', countSensor: 2 },
-  count3: { value: [7, 11, 13, 14], message: 'センサー 3個', countSensor: 3 },
-  count4: { value: [15], message: 'センサー 4個', countSensor: 4 },
+  count0: { value: [0], message: 'センサー 0個 \u{10008F}', countSensor: 0, stickerId: '52002761' },
+  count1: { value: [1, 2, 4, 8], message: "センサー 1個 \u{10008C}", countSensor: 1, stickerId: '52002762' },
+  count2: { value: [3, 5, 6, 9, 10, 12], message: 'センサー 2個 \u{10007A}', countSensor: 2, stickerId: '52002748' },
+  count3: { value: [7, 11, 13, 14], message: 'センサー 3個 \u{100091}', countSensor: 3, stickerId: '52002749' },
+  count4: { value: [15], message: 'センサー 4個 \u{1000A7}', countSensor: 4, stickerId: '52002756' },
 };
+
+const PackageId = '11537';
 
 const client = new line.Client(config);
 
@@ -46,41 +48,44 @@ function handleThingsEvent(event) {
   const payLoad = event.things.result.bleNotificationPayload;
   const buffPayLoad = Buffer.from(payLoad, 'base64');
   const countSensor = new Int8Array(buffPayLoad);
-  const message = jugmentCountSensor(countSensor[0])
-  const echo = { type: 'text', text: message };
-  return client.replyMessage(event.replyToken, echo)
+  const kindSensor = jugmentCountSensor(countSensor[0])
+  const echoMessage = { type: 'text', text: kindSensor.message };
+  const echoSticker = { type: 'sticker', packageId: PackageId, stickerId: kindSensor.stickerId }
+  // return client.replyMessage(event.replyToken, echo)
+  // client.replyMessage(event.replyToken, echoSticker);
+  client.replyMessage(event.replyToken, echoMessage);
 };
 
 function handleMessageEvent(event) {
   const echo = { type: 'text', text: 'Message Event' };
-  return client.replyMessage(event.replyToken, echo)
+  client.replyMessage(event.replyToken, echo)
 };
 
 function jugmentCountSensor(sensor) {
   if (BufferData.count0.value.indexOf(sensor) > -1) {
     if (currentSensor !== BufferData.count0.countSensor) {
       currentSensor = BufferData.count0.countSensor;
-      return (BufferData.count0.message)
+      return (BufferData.count0)
     }
   } else if (BufferData.count1.value.indexOf(sensor) > -1) {
     if (currentSensor !== BufferData.count1.countSensor) {
       currentSensor = BufferData.count1.countSensor;
-      return (BufferData.count1.message)
+      return (BufferData.count1)
     }
   } else if (BufferData.count2.value.indexOf(sensor) > -1) {
     if (currentSensor !== BufferData.count2.countSensor) {
       currentSensor = BufferData.count2.countSensor;
-      return (BufferData.count2.message)
+      return (BufferData.count2)
     }
   } else if (BufferData.count3.value.indexOf(sensor) > -1) {
     if (currentSensor !== BufferData.count3.countSensor) {
       currentSensor = BufferData.count3.countSensor;
-      return (BufferData.count3.message)
+      return (BufferData.count3)
     }
   } else if (BufferData.count4.value.indexOf(sensor) > -1) {
     if (currentSensor !== BufferData.count4.countSensor) {
       currentSensor = BufferData.count4.countSensor;
-      return (BufferData.count4.message)
+      return (BufferData.count4)
     }
   }
 }
