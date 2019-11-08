adbf493b20a5f00802071aa395fba44b3323df00
Taku Nakagawa <tk@www-biz.co>
2019-11-08 15:27:06 +0900

    センサー4つから０個になったときにスタンプを送付する。

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const config = {
 }
 
 const BufferData = {
-  count0: { value: [0], message: 'センサー 0個 \u{10008F}', countSensor: 0, stickerId: '52002761' },
-  count1: { value: [1, 2, 4, 8], message: "センサー 1個 \u{10008C}", countSensor: 1, stickerId: '52002762' },
-  count2: { value: [3, 5, 6, 9, 10, 12], message: 'センサー 2個 \u{10007A}', countSensor: 2, stickerId: '52002748' },
-  count3: { value: [7, 11, 13, 14], message: 'センサー 3個 \u{100091}', countSensor: 3, stickerId: '52002749' },
-  count4: { value: [15], message: 'センサー 4個 \u{1000A7}', countSensor: 4, stickerId: '52002756' },
+  count0: { value: [0], message: 'センサー 0個　取得できていますか？余裕です！ \u{10008F}', countSensor: 0, stickerId: '52002761' },
+  count1: { value: [1, 2, 4, 8], message: "センサー 1個　まだまだです！！ \u{10008C}", countSensor: 1, stickerId: '52002762' },
+  count2: { value: [3, 5, 6, 9, 10, 12], message: 'センサー 2個　半分です！ \u{10007A}', countSensor: 2, stickerId: '52002748' },
+  count3: { value: [7, 11, 13, 14], message: 'センサー 3個　いよいよです！ \u{100091}', countSensor: 3, stickerId: '52002749' },
+  count4: { value: [15], message: 'センサー 4個　トイレです！ \u{1000A7}', countSensor: 4, stickerId: '52002756' },
+  leaked: { value: 'leaked', message: 'もれた？', stickerId: '52002766' },
 };
 
-// const LineStickerPackageId = '11537';
+const LineStickerPackageId = '11537';
 
 const client = new line.Client(config);
 
@@ -34,7 +35,6 @@ app.get('/', (req, res) => {
 
 // Line developer Webhookの利用
 app.post('/callback', line.middleware(config), (req, res) => {
-
   res.sendStatus(200);
   Promise.all(req.body.events.map(handleEvent))
 });
@@ -46,7 +46,7 @@ function handleEvent(event) {
   } else {
     handleMessageEvent(event);
   }
-}
+};
 
 // Line ThingsからEvent時に反応
 function handleThingsEvent(event) {
@@ -54,7 +54,10 @@ function handleThingsEvent(event) {
   const buffPayLoad = Buffer.from(payLoad, 'base64');
   const countSensor = new Int8Array(buffPayLoad);
   const kindSensor = jugmentCountSensor(countSensor[0])
-  if (kindSensor !== undefined) {
+  if (kindSensor.value === 'leaked') {
+    const echoMessage = { type: 'sticker', packageId: LineStickerPackageId, stickerId: kindSensor.stickerId };
+    client.replyMessage(event.replyToken, echoMessage);
+  } else if (kindSensor !== undefined) {
     const echoMessage = { type: 'text', text: kindSensor.message };
     client.replyMessage(event.replyToken, echoMessage);
   }
@@ -68,7 +71,10 @@ function handleMessageEvent(event) {
 
 function jugmentCountSensor(sensor) {
   if (BufferData.count0.value.indexOf(sensor) > -1) {
-    if (currentSensor !== BufferData.count0.countSensor) {
+    if (currentSensor === 4) {
+      currentSensor = BufferData.count0.countSensor;
+      return BufferData.leaked;
+    } else if (currentSensor !== BufferData.count0.countSensor) {
       currentSensor = BufferData.count0.countSensor;
       return (BufferData.count0)
     }
@@ -93,6 +99,6 @@ function jugmentCountSensor(sensor) {
       return (BufferData.count4)
     }
   }
-}
+};
 
 console.log(`Server running at ${PORT}`);
